659a1581be95ac800c248bb5fbce452040f39e38
Karunya Chavan <143827963+KarunyaChavan@users.noreply.github.com>
2024-06-06 20:37:49 +0530

    Add files via upload

---
+++ try.py
@@ -0,0 +1,7 @@
+#For Chaining
+                    # j = 0 
+                    # while(j<self.M):#Traversing the whole table
+                    #     if((self._table[j].key)%self.M==key%self.M and self._table[j].chain==-1): #Checking for synonyms and empty chain fields
+                    #         self._table[j].chain = self.index
+                    #         break
+                    #     j+=1
